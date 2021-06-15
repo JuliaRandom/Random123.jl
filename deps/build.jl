ab83d899a1037781f5259ab99e80283bbdec5737
Sunoru <s@sunoru.com>
2021-06-14 23:44:23 -0400

    Use  to detect AES feature of CPU.

--- deps/build.jl
+++ deps/build.jl
@@ -1,14 +1,7 @@
 disabled() = get(ENV, "R123_DISABLE_AESNI", "") != ""
 
-const __m128i = NTuple{2, VecElement{UInt64}}
-has_aesni() = try
-    ccall(
-        "llvm.x86.aesni.aeskeygenassist", llvmcall, __m128i, (__m128i, UInt8),
-        __m128i((0x0123456789123450, 0x9876543210987654)), 0x1
-    ) ≡ __m128i((0x857c266f7c266e85, 0x2346382146382023))
-catch e
-    false
-end
+using CpuId
+has_aesni() = cpufeature(:AES)
 
 const filename = joinpath(dirname(@__FILE__), "aes-ni")
 isfile(filename) && rm(filename)
