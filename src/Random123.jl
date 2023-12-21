c1654e073c51036484da97f7bf802eb48856c18f
Sunoru <s@sunoru.com>
2023-12-20 20:28:44 -0500

    Skip checking aes-ni on Apple ARM platforms.

--- src/Random123.jl
+++ src/Random123.jl
@@ -30,19 +30,23 @@ include("philox.jl")
 export R123_USE_AESNI
 
 "True when AES-NI has been enabled."
-const R123_USE_AESNI = try
-    cmd = Base.julia_cmd()
-    push!(
-        cmd.exec, "-e",
-        "const __m128i = NTuple{2, VecElement{UInt64}};" *
-        "@assert ccall(\"llvm.x86.aesni.aeskeygenassist\", " *
-        "llvmcall, __m128i, (__m128i, UInt8), " *
-        "__m128i((0x0123456789123450, 0x9876543210987654)), 0x1) ≡ " *
-        "__m128i((0x857c266f7c266e85, 0x2346382146382023))"
-    )
-    success(cmd)
-catch e
+const R123_USE_AESNI = @static if Sys.isapple() && Sys.ARCH ≡ :aarch64
     false
+else
+        try
+        cmd = Base.julia_cmd()
+        push!(
+            cmd.exec, "-e",
+            "const __m128i = NTuple{2, VecElement{UInt64}};" *
+            "@assert ccall(\"llvm.x86.aesni.aeskeygenassist\", " *
+            "llvmcall, __m128i, (__m128i, UInt8), " *
+            "__m128i((0x0123456789123450, 0x9876543210987654)), 0x1) ≡ " *
+            "__m128i((0x857c266f7c266e85, 0x2346382146382023))"
+        )
+        success(cmd)
+    catch e
+        false
+    end
 end
 
 @static if R123_USE_AESNI
@@ -52,7 +56,7 @@ end
     include("./aesni.jl")
     include("./ars.jl")
 else
-    @warn "AES-NI is not enabled, so AESNI and ARS are not available."
+    @warn "AES-NI instruction set is not enabled, so the related RNGs (AESNI and ARS) are not available."
 end
 
 end
